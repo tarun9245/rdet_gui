2b71498f36f1883b415f89202bde5e5f5b9a6b5b
Tarun Kumar (Temp) <tarkum@qti.qualcomm.com>
2021-06-14 15:26:23 +0530

    Added functionality to rdetlogshandler class

--- rdet_gui/rdetlogshandler.cpp
+++ rdet_gui/rdetlogshandler.cpp
@@ -1,6 +1,67 @@
 #include "rdetlogshandler.h"
+#include <QDebug>
+#include <QDateTime>
+#include <QHostInfo>
 
 RdetLogsHandler::RdetLogsHandler()
 {
 
+}
+
+RdetLogsHandler::RdetLogsHandler(QString filePath)
+{
+    file.setFileName(filePath + "\\RDET_Logs.txt");
+    if(!file.open(QIODevice::ReadWrite | QIODevice::Text | QIODevice::Append)) {
+        qDebug()<<"Unable to create RDET logs file at given loation. "<<file.fileName();
+        return;
+    }
+}
+
+void RdetLogsHandler:: setLogFilePath(QString path)
+{
+    file.setFileName(path + "\\RDET_Logs.txt");
+    if(!file.open(QIODevice::ReadWrite | QIODevice::Text | QIODevice::Append)) {
+        qDebug()<<"Unable to create RDET logs file at given loation. "<<file.fileName();
+        return;
+    }
+}
+
+void RdetLogsHandler::save()
+{
+    file.close();
+}
+
+void RdetLogsHandler::insertLine(QString line)
+{
+    QTextStream out(&file);
+    out<<line<<endl;
+}
+
+void RdetLogsHandler::insertFile(QString fileToCopy)
+{
+    QTextStream out(&file);
+    QFile temp(fileToCopy);
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug()<<"Unable to open given file. "<<temp.fileName();
+        return;
+    }
+    QTextStream read(&temp);
+    while(!read.atEnd()) {
+        QString line = read.readLine();
+        out<<line<<endl;
+    }
+
+}
+
+void RdetLogsHandler::formatLogFile()
+{
+    QTextStream out(&file);
+    out<<"\n======Begin RDET Logs=====\n";
+    QString date = QDate::currentDate().toString();
+    QString time = QTime::currentTime().toString();
+    QString host = QHostInfo::localHostName();
+    qDebug()<<"Host Name: "<<host<<endl;
+    qDebug()<<"today date is "<<date<<" and time is "<<time;
+    out<<"Starting RDET at "<<time<<" on "<<date<<endl;
+    out<<"Host Name: "<<host<<endl;
 }
--- rdet_gui/rdetlogshandler.h
+++ rdet_gui/rdetlogshandler.h
@@ -1,11 +1,20 @@
 #ifndef RDETLOGSHANDLER_H
 #define RDETLOGSHANDLER_H
-
+#include <QString>
+#include<QFile>
+#include <QTextStream>
 
 class RdetLogsHandler
 {
+    QFile file;
 public:
     RdetLogsHandler();
+    RdetLogsHandler(QString filePath);
+    void setLogFilePath(QString path);
+    void insertLine(QString line);
+    void insertFile(QString fileToCopy);
+    void formatLogFile();
+    void save();
 };
 
 #endif // RDETLOGSHANDLER_H
